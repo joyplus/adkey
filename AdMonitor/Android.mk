3959451fd428aa316169de9a85873033ec075835
jaselisan <jas.sheng@joyplus.tv>
2014-01-02 15:56:34 +0800

    Update AdMonitor.

--- AdMonitor/Android.mk
+++ AdMonitor/Android.mk
@@ -6,27 +6,17 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_STATIC_JAVA_LIBRARIES := \
         Miaozhen \
         fasthttp \
- #       libs/android-support-v4 \
 
 LOCAL_SRC_FILES := \
         $(call all-java-files-under, src)\
         src/com/joyplus/ad/config/adconfig.properties
 
-LOCAL_MODULE := adboot
+LOCAL_MODULE := admonitor
 #LOCAL_MODULE_CLASS := JAVA_LIBRARIES
 
 LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES:=Miaozhen:libs/Miaozhen.jar\
                                       fasthttp:libs/fasthttp.jar
-                                      #android-support-v4:libs/android-support-v4
+                                
 
 
 include $(BUILD_STATIC_JAVA_LIBRARY)
-
-# additionally, build tests in sub-folders in a separate .apk
-# include $(call all-makefiles-under,$(LOCAL_PATH))
-
-#include $(CLEAR_VARS)
-#LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES:=Miaozhen:libs/Miaozhen.jar\
-#                                      fasthttp:libs/fasthttp.jar
-#                                      #android-support-v4:libs/android-support-v4
-#include $(BUILD_MULTI_PREBUILT)
