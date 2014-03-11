8ec4017c5d37a083c460f33ae73500e7082227cc
jaselisan <jas.sheng@joyplus.tv>
2014-03-11 10:32:58 +0800

    update admonitor config

--- AdMonitor/src/com/joyplus/admonitor/Application/AdMonitorConfig.java
+++ AdMonitor/src/com/joyplus/admonitor/Application/AdMonitorConfig.java
@@ -10,11 +10,11 @@ import android.content.Context;
  * define by Jas@20140102*/
 public class AdMonitorConfig {
 	
-    private static boolean mAdBootDebugEnable   = true;
+    private static boolean mAdBootDebugLogEnable   = true;
     
-    private final  static  String mCompany      = "joyplus.adkey";
-    private final  static  String mBaseURL      = "D5313CEAA0E07E98E875117C601ABF26D0248FA1ED3572ACAE7D7F3BBCD40861110294B2C80228A23BBB1702421FFB63C0857980F9D4EE9174A9ABD4EDFBE4C14585B0F8379DFDEFB14531E5B7C4FC0FFE41A3942912F60A";
-    private final  static  String mDebugBaseURL = "D5313CEAA0E07E98E875117C601ABF26D0248FA1ED3572AC5F31E91461BF151ADDD7BE9F4CAD1C0BA299B9BAC9EF1545C0857980F9D4EE9174A9ABD4EDFBE4C14585B0F8379DFDEFB14531E5B7C4FC0FFE41A3942912F60A";
+    private final  static  String mCompany         = "joyplus.adkey";
+    private final  static  String mBaseURL         = "D5313CEAA0E07E98E875117C601ABF26D0248FA1ED3572ACAE7D7F3BBCD40861110294B2C80228A23BBB1702421FFB63C0857980F9D4EE9174A9ABD4EDFBE4C14585B0F8379DFDEFB14531E5B7C4FC0FFE41A3942912F60A";
+    private final  static  String mDebugBaseURL    = "D5313CEAA0E07E98E875117C601ABF26D0248FA1ED3572AC5F31E91461BF151ADDD7BE9F4CAD1C0BA299B9BAC9EF1545C0857980F9D4EE9174A9ABD4EDFBE4C14585B0F8379DFDEFB14531E5B7C4FC0FFE41A3942912F60A";
     //add by Jas@20140305 for app and vc collect
     private final  static  String mAppBaseURL      = "D5313CEAA0E07E98E875117C601ABF26D0248FA1ED3572ACAE7D7F3BBCD40861110294B2C80228A23BBB1702421FFB63C0857980F9D4EE9174A9ABD4EDFBE4C14585B0F8379DFDEF708A5EDF6E008A9C0375DE907C2BB3EA";
     private final  static  String mVcBaceURL       = "D5313CEAA0E07E98E875117C601ABF26D0248FA1ED3572ACAE7D7F3BBCD40861110294B2C80228A23BBB1702421FFB63C0857980F9D4EE9174A9ABD4EDFBE4C14585B0F8379DFDEF16AF2741D452255050C107AA4488E854";
@@ -38,10 +38,10 @@ public class AdMonitorConfig {
 		    mVcURL  = mDes.strDec(mVcBaceURL, GetCompany(), "", "");
 		}
 		if(AdMonitorSDKFeature.EXTERNAL_CONFIG){
-			mAdBootDebugEnable  = AdMonitorExternalConfig.getInstance().GetDebugEnable(mAdBootDebugEnable);
-			mURL                = AdMonitorExternalConfig.getInstance().GetBaseURL(mURL);
-			mAppURL             = AdMonitorExternalConfig.getInstance().GetAppURL(mAppURL);
-			mVcURL              = AdMonitorExternalConfig.getInstance().GetVcURL(mVcURL);
+			mAdBootDebugLogEnable  = AdMonitorExternalConfig.getInstance().GetDebugEnable(mAdBootDebugLogEnable);
+			mURL                   = AdMonitorExternalConfig.getInstance().GetBaseURL(mURL);
+			mAppURL                = AdMonitorExternalConfig.getInstance().GetAppURL(mAppURL);
+			mVcURL                 = AdMonitorExternalConfig.getInstance().GetVcURL(mVcURL);
 		}
 	}
     
@@ -49,13 +49,15 @@ public class AdMonitorConfig {
 	public static String ToString(){
 		return ("AdBootConfig {"
 				+" ,mAdBootDebugEnable="+GetDebugEnable()
-				+" ,mBaseURL="+GetBaseURL()
+				+" ,mURL="+GetBaseURL()
 				+" ,mCompany="+GetCompany()
+				+" ,mAppURL="+GetAppURL()
+				+" ,mVcURL="+GetVcURL()
 				+" }");
 	}
 	/*Interface of Application*/
 	public static boolean GetDebugEnable(){ 
-		return mAdBootDebugEnable;
+		return mAdBootDebugLogEnable;
 	}
 	
 	public static String GetBaseURL(){
